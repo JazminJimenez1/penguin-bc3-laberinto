ae68e97a4988eedfb55a9790f7521b78258021c7
JazminJimenez1 <sol.bencas@gmail.com>
2019-05-13 18:53:45 +0200

    laberintodos

--- diasiete.py
+++ diasiete.py
@@ -1,2 +1,5 @@
+#remote area:git push
+#stage area:git comitt in 
+#untage:git add
 #Practica con control de versiones git
 print("hola que tal")
---
+++ diasiete2.py
@@ -0,0 +1,10 @@
+#git add .
+#git commit -m "el mensaje"
+#git push  (para empujar el codigo del local al remoto)
+#git pull (para estirar el codigo del remoto al local)
+#
+#COMANDOS PARA NAVEGAR EN LA TERMINAL
+#
+#ls o dir                     (para ver las carpetas/archivos existentes)
+#cd nombre_de_archivo         (para entrar en la carpeta/archivos)
+#cd ..                        (para salir de los archivos)
